e05156476bcac609215fd0caf2602c9fcb3c3f77
Lalit <nankarlait7@gmail.com>
2023-12-26 22:35:50 +0530

    Medicine Page Initial Commit

---
+++ src/Component/Medicine.jsx
@@ -0,0 +1,138 @@
+import './Medicine.css'
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import pic1 from './Image/brow.jpg';
+import pic2 from './Image/healthcare3.jpg';
+import pic3 from './Image/healthcare.jpg';
+import pic4 from './Image/medicine-bottles-pills4.jpg';
+import pic5 from './Image/packings-pills-capsules-medicines_5.jpg';
+import pic6 from './Image/colorful-pills8.jpg';
+export function Medicine(){
+  return(
+  <Container>
+    <Row>
+       <Col>
+    <Card className ="mt-5" style={{ width: '18rem' }} >
+    <Card.Img variant="top" src={pic2} />
+    <Card.Body className='CardBody'>
+      <Card.Title className='CardTitle' >Paracetamol</Card.Title>
+      <Card.Text className="CardText">
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+    </Card.Body>
+    <ListGroup className="list-group-flush">
+      <ListGroup.Item>Cras justo odio</ListGroup.Item>
+      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+      <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+    </ListGroup>
+   
+  </Card>
+ </Col> 
+
+ <Col>
+    <Card className="mt-5" style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={pic6} />
+    <Card.Body className='CardBody'>
+      <Card.Title className='CardTitle'>Dolutegravir</Card.Title>
+      <Card.Text className="CardText">
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+    </Card.Body>
+    <ListGroup className="list-group-flush">
+      <ListGroup.Item>Cras justo odio</ListGroup.Item>
+      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+      <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+    </ListGroup>
+   
+  </Card>
+ </Col> 
+
+ <Col>
+    <Card className="mt-5" style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={pic1} />
+    <Card.Body  className='CardBody'>
+      <Card.Title className='CardTitle'>Mupirocin</Card.Title>
+      <Card.Text className="CardText">
+      Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+    </Card.Body>
+    <ListGroup className="list-group-flush">
+      <ListGroup.Item>Cras justo odio</ListGroup.Item>
+      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+      <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+    </ListGroup>
+   
+  </Card>
+ </Col> 
+  </Row>
+
+  <Row>
+       <Col>
+    <Card className="mt-5" style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={pic4} />
+    <Card.Body className='CardBody'>
+      <Card.Title className='CardTitle' >Delamanid</Card.Title>
+      <Card.Text className="CardText">
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+    </Card.Body>
+    <ListGroup className="list-group-flush">
+      <ListGroup.Item>Cras justo odio</ListGroup.Item>
+      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+      <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+    </ListGroup>
+   
+  </Card>
+ </Col> 
+
+ <Col>
+    <Card className="mt-5" style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={pic3} />
+    <Card.Body className='CardBody'>
+      <Card.Title className='CardTitle'>Bedaquiline</Card.Title>
+      <Card.Text className="CardText">
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+    </Card.Body>
+    <ListGroup className="list-group-flush">
+      <ListGroup.Item>Cras justo odio</ListGroup.Item>
+      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+      <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+    </ListGroup>
+   
+  </Card>
+ </Col> 
+
+ <Col>
+    <Card className="mt-5" style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={pic5} />
+    <Card.Body  className='CardBody'>
+      <Card.Title className='CardTitle'>Terbinafine</Card.Title>
+      <Card.Text className="CardText">
+      an analegisc and to antiprytic is used to temporaly relieve in pain the
+        bulk of the card's content.
+      </Card.Text>
+    </Card.Body>
+    
+     
+    <ListGroup className="list-group-flush">
+      <ListGroup.Item>Cras justo odio</ListGroup.Item>
+      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+      <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+    </ListGroup>
+   
+   
+  </Card>
+ </Col> 
+  </Row>
+  </Container>
+  )
+}
